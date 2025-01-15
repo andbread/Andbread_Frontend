a8de528960cdb77b54680938ad35f3d190d1be26
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2025-01-15 19:00:21 +0900

    supabase init

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { clientCreate } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+export const supabase = clientCreate(supabaseUrl,supabaseKey);
