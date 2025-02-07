90effd603e324e6fe7e0747b72efc69de40d6995
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2025-02-07 19:30:31 +0900

    refactor : 라우팅 디렉토리 경로 변경 src/pages -> src/app

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginButton from "@/components/login/LoginButton";
+const LoginPage = () => {
+    return (
+        <>
+            로그인 페이지 입니다.
+            <br/>
+            <LoginButton/>
+        </>
+    )
+}
+export default LoginPage;
---
+++ src/components/login/LoginButton.tsx
@@ -0,0 +1,8 @@
+const LoginButton = () => {
+    return (
+        <>
+        로그인버튼입니다옹
+        </>
+    )
+}
+export default LoginButton;
