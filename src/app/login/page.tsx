bce7c68056127d163b7fbede4fff22af7c792ee5
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2025-02-09 20:06:02 +0900

    로그인 페이지 퍼블리싱 완료/ 추후 로고 넣기

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,15 @@
 import LoginButton from '@/components/login/LoginButton'
 const LoginPage = () => {
   return (
-    <div >
-      <br />
+    <div>
+        <div className='w-100% h-500 flex justify-center items-center'>
+            <h1>로고</h1>
+        </div>
+    <div className={`flex flex-col gap-8 items-center justify-center`}>
       <LoginButton provider="kakao" />
       <LoginButton provider="google" />
     </div>
+    </div>
   )
 }
-export default LoginPage
+export default LoginPage;
--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -1,23 +1,32 @@
+import Icon from "../common/icon/Icon";
 type SocialProvider = "kakao" | "google";
 interface LoginButtonProps {
     provider : SocialProvider;  
 }
+const providerStyles = {
+    kakao : " bg-[#FEE500] hover:bg-yellow-400 text-black",
+    google: "bg-[#F2F2F2]  hover:bg-gray-200 text-black shadow-lg",
+}
+const providerText = {
+    google: "구글로 시작하기",
+    kakao: "카카오로 시작하기",
+  }
 const LoginButton = ({provider} : LoginButtonProps) => {
-    const providerStyles = {
-        kakao : " bg-yellow-400 hover:bg-yellow-500 text-black",
-        google: "bg-[#F2F2F2] hover:bg-[#FF5733] text-black",
-    }
-    const providerText = {
-        google: "구글 로그인",
-        kakao: "카카오 로그인",
-      };
+   
     return (
         <button
-        className={` rounded ${providerStyles[provider]}`}
-        style={{ width: "272px", height: "62px" }} 
+        className={`w-[272px] h-[62px]
+             py-2 rounded
+              ${providerStyles[provider]}
+               text-heading04
+                flex justify-center items-center
+                transition-all duration-300
+                rounded-lg
+                `}
+        >
+            <Icon type={provider=== "kakao" ? "kakaoLogo" : "googleLogo"} width={16} height={16}/>
+            <h4 className="w-214 h-20">{providerText[provider]}</h4>
         
-      >
-        {providerText[provider]}
       </button>
     )
 }
