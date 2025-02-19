04ed1af7539dc70748ae2ca18c12988ff19e08a6
shinhm1 <shinhm1@naver.com>
2025-02-19 17:29:25 +0900

    chore: NEXT Image 도메인 및 classnames, react-hook-form 라이브러리 추가

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
     })
     return config
   },
+  // TODO 구글, 카카오 프로필 이미지 도메인 추가 필요
+  images: {
+    domains: ['avatars.githubusercontent.com'], // TODO 프로필 이미지 테스트용 임시 도메인, 수정 필요
+  },
 }
 
 export default nextConfig
