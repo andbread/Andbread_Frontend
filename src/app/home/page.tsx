43cf9c658c2ecfd095fd25b69008ba8bc2203f28
KangBoSeok <kbs9901@gmail.com>
2025-02-20 18:18:20 +0900

    feat : 기본 홈 페이지 퍼블리싱

---
+++ src/app/home/page.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/home/Header";
+import EmptyLog from "@/components/home/Emptylog";
+import AddLogButton from "@/components/home/AddLogButton";
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col justify-between p-24 pt-16">
+      <Header />
+      <main className="p-4 mt-24">
+        <section>
+          <h2 className="text-lg font-bold mb-24">이번 달 엔빵</h2>
+          <EmptyLog />
+        </section>
+
+        <section className="mt-40">
+          <h2 className="text-lg font-bold mb-24">나의 엔빵</h2>
+          <AddLogButton/>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
