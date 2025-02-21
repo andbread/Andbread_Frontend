de9ea42ee08c3e8ad9fe8b8798889d47cd9e9ca9
KangBoSeok <kbs9901@gmail.com>
2025-02-21 10:11:29 +0900

    style : 글씨 색깔 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,15 +5,15 @@ import AddLogButton from "@/components/home/AddLogButton";
 const HomePage = () => {
   return (
     <div className="flex flex-col justify-between p-24 pt-16">
-      <Header />
+      <Header /> 
       <main className="p-4 mt-24">
         <section>
-          <h2 className="text-lg font-bold mb-24">이번 달 엔빵</h2>
+          <h2 className="text-lg font-bold mb-24 text-[#333036]">이번 달 엔빵</h2>
           <EmptyLog />
         </section>
 
         <section className="mt-40">
-          <h2 className="text-lg font-bold mb-24">나의 엔빵</h2>
+          <h2 className="text-lg font-bold mb-24 text-[#333036]">나의 엔빵</h2>
           <AddLogButton/>
         </section>
       </main>
--- src/components/home/Emptylog.tsx
+++ src/components/home/Emptylog.tsx
@@ -5,8 +5,8 @@ interface EmptyLogProps {
   const EmptyLog = ({ message = "등록된 엔빵이 없어요 🍞" }: EmptyLogProps) => {
     return (
       <div className="bg-gray-300 p-4 py-36 rounded-xl shadow-md">
-        <p className="text-[12px] text-gray-900 ml-48">{message}</p>
-        <p className="text-[16px] font-bold mt-10 ml-48">엔빵을 추가해보세요!</p>
+        <p className="text-[12px] text-[#333036] ml-48">{message}</p>
+        <p className="text-[16px] font-bold text-[#333036] mt-10 ml-48">엔빵을 추가해보세요!</p>
       </div>
     );
   };
