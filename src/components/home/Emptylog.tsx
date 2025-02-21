137149eca5c503ab12f21ad52af7295552156a88
KangBoSeok <kbs9901@gmail.com>
2025-02-21 10:15:56 +0900

    style : 배경색 변경

--- src/components/home/Emptylog.tsx
+++ src/components/home/Emptylog.tsx
@@ -4,12 +4,12 @@ interface EmptyLogProps {
   
   const EmptyLog = ({ message = "등록된 엔빵이 없어요 🍞" }: EmptyLogProps) => {
     return (
-      <div className="bg-gray-300 p-4 py-36 rounded-xl shadow-md">
+      <div className="bg-[#E5E5E5] p-4 py-36 rounded-xl shadow-md">
         <p className="text-[12px] text-[#333036] ml-48">{message}</p>
         <p className="text-[16px] font-bold text-[#333036] mt-10 ml-48">엔빵을 추가해보세요!</p>
       </div>
     );
-  };
+  }; 
   
   export default EmptyLog;
   
