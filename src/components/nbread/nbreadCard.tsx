15926a4f09122ccc48c2bbef143444892d393b52
shinhm1 <shinhm1@naver.com>
2025-02-25 17:23:11 +0900

    fix: NbreadCard에서 data가 null일 경우 UI 추가

--- src/components/nbread/nbreadCard.tsx
+++ src/components/nbread/nbreadCard.tsx
@@ -1,40 +1,50 @@
 import { Nbread } from '@/types/nbread'
 
 interface NbreadCardProps {
-  nbreadData: Nbread
+  nbreadData: Nbread | null
 }
 
 const NbreadCard = ({ nbreadData }: NbreadCardProps) => {
   return (
     <>
-      <div className="card px-24 pb-32">
-        <div className="pb-4 text-body03 text-gray-600">총 금액</div>
-        <div className="text-heading01 text-secondary-200">
-          {Number(nbreadData.amount).toLocaleString()}원
-        </div>
-        <hr className="mt-20 border-gray-100" />
-        <div className="flex flex-col gap-16 pt-20">
-          <div className="flex flex-row items-center justify-between">
-            <div className="w-120 text-body02 text-gray-500">참여 인원</div>
-            <div className="text-body02 text-gray-800">
-              {nbreadData.participantCount}명
+      <div className="card min-h-200 px-24 pb-32">
+        {nbreadData ? (
+          <>
+            <div className="pb-4 text-body03 text-gray-600">총 금액</div>
+            <div className="text-heading01 text-secondary-200">
+              {Number(nbreadData.amount).toLocaleString()}원
             </div>
-          </div>
-          <div className="flex flex-row items-center justify-between">
-            <div className="w-120 text-body02 text-gray-500">엔빵 금액</div>
-            <div className="text-body02 text-gray-800">
-              {nbreadData.paymentAmount.toLocaleString()}원
+            <hr className="mt-20 border-gray-100" />
+            <div className="flex flex-col gap-16 pt-20">
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-120 text-body02 text-gray-500">참여 인원</div>
+                <div className="text-body02 text-gray-800">
+                  {nbreadData.participantCount}명
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-120 text-body02 text-gray-500">엔빵 금액</div>
+                <div className="text-body02 text-gray-800">
+                  {nbreadData.paymentAmount.toLocaleString()}원
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div className="w-120 text-body02 text-gray-500">
+                  정기 결제일
+                </div>
+                <div className="text-body02 text-gray-800">
+                  {nbreadData.paymentPeriod === 'year'
+                    ? `매년 ${nbreadData.paymentMonth}월 ${nbreadData.paymentDate}일`
+                    : `매월 ${nbreadData.paymentDate}일`}
+                </div>
+              </div>
             </div>
+          </>
+        ) : (
+          <div className="text-body02 text-gray-400">
+            엔빵 데이터 저장 중...
           </div>
-          <div className="flex flex-row items-center justify-between">
-            <div className="w-120 text-body02 text-gray-500">정기 결제일</div>
-            <div className="text-body02 text-gray-800">
-              {nbreadData.paymentPeriod === 'year'
-                ? `매년 ${nbreadData.paymentMonth}월 ${nbreadData.paymentDate}일`
-                : `매월 ${nbreadData.paymentDate}일`}
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </>
   )
