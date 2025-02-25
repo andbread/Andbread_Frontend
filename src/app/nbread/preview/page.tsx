e1ee4849497b1c9f6addaf08d72a7963b3437a21
shinhm1 <shinhm1@naver.com>
2025-02-25 17:24:12 +0900

    feat: 엔빵 등록 기능 및 등록 시 Toast 메시지 출력 기능 구현

--- src/app/nbread/preview/page.tsx
+++ src/app/nbread/preview/page.tsx
@@ -6,9 +6,34 @@ import NbreadCard from '@/components/nbread/nbreadCard'
 import NbreadParticipantCard from '@/components/nbread/nbreadParticipantCard'
 import useNbreadStore from '@/stores/useNbreadStore'
 import { Nbread } from '@/types/nbread'
+import { insertNbread } from '@/lib/nbread'
+import { insertParticipant } from '@/lib/participant'
+import { useToast } from '@/components/common/toast/Toast'
+import { useRouter } from 'next/navigation'
+import useUserStore from '@/stores/useAuthStore'
 
 const Page = () => {
   const nbreadData = useNbreadStore((state) => state.nbread)
+  const userData = useUserStore((state) => state.user)
+  const { clearNbread } = useNbreadStore()
+  const router = useRouter()
+
+  const handleClickSubmitButton = async () => {
+    if (nbreadData && nbreadData.participants) {
+      try {
+        const newNbreadId = await insertNbread(nbreadData!)
+        await insertParticipant(nbreadData.participants[0], newNbreadId)
+
+        clearNbread()
+        useToast.success('엔빵 만들기에 성공했어요.')
+        router.push('/home')
+      } catch (error) {
+        useToast.error('엔빵 만들기에 실패했어요. 다시 시도해주세요.')
+        console.error(error)
+        router.push('/nbread/create')
+      }
+    }
+  }
 
   return (
     <main className="h-full-y-auto relative p-24">
@@ -17,12 +42,16 @@ const Page = () => {
         <div className="pt-20 text-body03 text-gray-400">
           작성한 엔빵의 미리보기에요.
         </div>
-        <h2 className="pb-12 pt-4">{nbreadData?.title}</h2>
-        <NbreadCard nbreadData={nbreadData as Nbread} />
+        <h2 className="pb-12 pt-4">{nbreadData?.title || '저장 중...'}</h2>
+        {<NbreadCard nbreadData={nbreadData as Nbread} />}
         <div className="mb-12 mt-40 text-body02 text-gray-500">참여한 사람</div>
         <div className="mb-40 flex flex-col gap-8">
           {/* TODO 로그인 기능 구현 후 데이터 수정 필요 */}
-          <NbreadParticipantCard isNbreadLeader={true} name={'신혜민'} />
+          <NbreadParticipantCard
+            isNbreadLeader={true}
+            name={userData!.name}
+            profileImageUrl={userData!.profileImage}
+          />
           <DashlineCard
             text="친구는 엔빵을 만든 후 초대할 수 있어요."
             iconType="warning"
@@ -32,7 +61,7 @@ const Page = () => {
         </div>
         <button
           className="btn btn-large btn-primary mb-20"
-          onClick={() => null}
+          onClick={() => handleClickSubmitButton()}
         >
           엔빵 만들기 🍞
         </button>
