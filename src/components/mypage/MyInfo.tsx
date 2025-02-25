edcde60581cd5e7f86747ed785a8ab069070372d
KangBoSeok <kbs9901@gmail.com>
2025-02-25 13:36:03 +0900

    feat : 계정 정보 불러오기 기능

--- src/components/mypage/MyInfo.tsx
+++ src/components/mypage/MyInfo.tsx
@@ -1,30 +1,26 @@
-"use client"
+"use client";
 
-import Avatar from '../../components/common/avatar/avatar';
+import Avatar from "../../components/common/avatar/avatar";
+import useUserStore from "@/stores/useAuthStore"; 
 
-interface MyInfoProps {
-  profileImageUrl: string; 
-}
+const MyInfo = () => {
+  const { user } = useUserStore(); 
 
-const MyInfo = ({ profileImageUrl }: MyInfoProps) => {
   return (
     <div className="card mt-12">
-      <div className="flex items-center justify-between mr-12">
-        <div>
-        <Avatar
-          profileImageUrl={profileImageUrl}
-          size="large"
-        />
-        </div>
-        <div className='flex-shrink-0 justify-between mr-225 ml-12'>
-          <p className="font-bold mb-5 text-heading05 text-gray-800">신혜민</p>
-          <p className="text-body04 text-gray-600 whitespace-nowrap">카카오 계정 
-            <span className="text-body04 text-gray-400 ml-12 mr-36 whitespace-nowrap"> asdf1234@naver.com</span>
-          </p> 
+      <div className="flex items-center justify-between px-6 py-4">
+        <Avatar profileImageUrl={user?.profileImage || "/default-avatar.png"} size="large" />
+
+        <div className="flex-shrink-0 justify-between mr-225 ml-12">
+          <p className="font-bold mb-5 text-heading05 text-gray-800">{user?.name || "이름 없음"}</p>
+          <p className="text-body04 text-gray-600 whitespace-nowrap">
+            {user?.socialType?.toUpperCase() || "소셜 로그인"} 계정
+            <span className="text-body04 text-gray-400 ml-12 mr-36 whitespace-nowrap">{user?.email || "이메일 없음"}</span>
+          </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default MyInfo;
