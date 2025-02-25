453f2020094c0342bfe6cbab07cf02acd8559c17
shinhm1 <shinhm1@naver.com>
2025-02-25 17:21:04 +0900

    feat: lib 내 디렉토리 생성 및 supabase insert, delete 메소드 구현

---
+++ src/lib/nbread/deleteNbread.ts
@@ -0,0 +1,19 @@
+import { supabase } from '@/lib/supabaseClient'
+
+export const insertNbread = async (nbreadId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('nbread')
+      .delete()
+      .eq('id', nbreadId)
+
+    if (error) {
+      console.error('Error inserting nbread:', error)
+      throw error
+    }
+
+    return data
+  } catch (error) {
+    throw error
+  }
+}
