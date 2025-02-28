8f7d70ffbda1159843e1a9e4d16925c535e9b66d
shinhm1 <shinhm1@naver.com>
2025-02-28 13:22:24 +0900

    fix: 홈페이지 수정

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,49 +1,61 @@
-"use client";
-import { useEffect, useState } from "react";
-import { getUserNbreads } from "@/lib/nbread/getUserNbread";
-import Header from "@/components/home/Header";
-import MonthlyNbread from "@/components/home/MonthlyNbread";
-import MyNbread from "@/components/home/MyNbread";
+'use client'
+import { useEffect, useState } from 'react'
+import { getUserNbreads } from '@/lib/nbread/getUserNbread'
+import Header from '@/components/home/Header'
+import MonthlyNbread from '@/components/home/MonthlyNbread'
+import MyNbread from '@/components/home/MyNbread'
+import { Nbread } from '@/types/nbread'
+import useUserStore from '@/stores/useAuthStore'
+import { getParticipants } from '@/lib/participant/getParticipants'
 
 const HomePage = () => {
-  const [nbreadList, setNbreadList] = useState([]);
-  const [userId, setUserId] = useState<string | null>(null);
-  const [totalAmount, setTotalAmount] = useState(0);
-  const [currentMonth, setCurrentMonth] = useState<number>(new Date().getMonth() + 1);
+  const user = useUserStore((state) => state.user)
+  const [nbreadList, setNbreadList] = useState<Nbread[]>([])
+  const [totalAmount, setTotalAmount] = useState(0)
+
+  const currentMonth = new Date().getMonth() + 1
+
+  // Nbread 및 Participant 정보를 DB로부터 fetch
+  const fetchNbreads = async (userId: string) => {
+    const nbreads = await getUserNbreads(userId)
+
+    const nbreadsWithParticipants = await Promise.all(
+      nbreads.map(async (nbread) => {
+        const participants = await getParticipants(nbread.id)
+        return { ...nbread, participants }
+      }),
+    )
+    setNbreadList(nbreadsWithParticipants)
+  }
 
   useEffect(() => {
-    const userStore = sessionStorage.getItem("user-store");
-    if (userStore) {
-      const userData = JSON.parse(userStore).state?.user;
-      if (userData?.id) {
-        setUserId(userData.id);
-      }
-    }
-  }, []);
+    if (!user) return
+    fetchNbreads(user.id)
+  }, [user])
 
+  // NbreadList가 업데이트된 후 totalAmount 계산
   useEffect(() => {
-    if (!userId) return;
-    const fetchNbreads = async () => {
-      const nbreads = await getUserNbreads(userId);
-      setNbreadList(nbreads);
-      const total = nbreads.reduce(
-        (sum, nbread) => sum + Math.floor(nbread.amount / Math.max(nbread.participant_count, 1)),
-        0
-      );
-      setTotalAmount(total);
-    };
-    fetchNbreads();
-  }, [userId]);
+    const total = nbreadList.reduce(
+      (sum: number, nbread: Nbread) =>
+        sum + Math.floor(nbread.amount / Math.max(nbread.participantCount, 1)),
+      0,
+    )
+    setTotalAmount(total)
+  }, [nbreadList])
 
   return (
     <div className="flex flex-col justify-between p-24 pt-16">
       <Header />
-      <main className="p-4 mt-24">
-        <MonthlyNbread nbreadList={nbreadList} totalAmount={totalAmount} currentMonth={currentMonth} />
+      <main className="mt-24 p-4">
+        <MonthlyNbread
+          nbreadList={nbreadList}
+          totalAmount={totalAmount}
+          currentMonth={currentMonth}
+        />
         <MyNbread nbreadList={nbreadList} />
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
