fb5b69ef76ec92976f1fb4067866390fbc5bccc8
shinhm1 <shinhm1@naver.com>
2025-02-28 15:54:26 +0900

    fix: LogoutModal 텍스트 및 간격 수정

--- src/components/user/LogoutModal.tsx
+++ src/components/user/LogoutModal.tsx
@@ -1,34 +1,40 @@
-import Modal from "../common/Modal/Modal";
+import Modal from '../common/Modal/Modal'
 
 interface LogoutModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    onSubmit: () => void; 
+  isOpen: boolean
+  onClose: () => void
+  onSubmit: () => void
 }
 
 const LogoutModal = ({ isOpen, onClose, onSubmit }: LogoutModalProps) => {
-    return (
-        <Modal isOpen={isOpen} onClose={onClose}>
-            <div className="flex flex-col p-8">
-                <h2 className="text-heading05 font-semibold text-gray-800 mb-10 ">로그아웃 하시겠습니까?</h2>
-                <p className="text-body02 text-gray-600 mb-30 ">로그아웃을 진행하면, 다시 로그인해야합니다.</p>
-                <div className="flex flex-row gap-10 ">
-                <button
-                    onClick={onClose}
-                    className="bg-gray-300 text-gray-800 rounded-md btn-small text-heading06"
-                >
-                    취소
-                </button>
-                <button
-                    onClick={onSubmit} 
-                    className="bg-red-500 text-white rounded-md btn-small text-heading06 "
-                >
-                    로그아웃
-                </button>
-                </div>
-            </div>
-        </Modal>
-    );
-};
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="flex flex-col p-8">
+        <div className="flex flex-col pl-8">
+          <h2 className="mb-10 text-heading05 font-semibold text-gray-800">
+            로그아웃 하시겠어요?
+          </h2>
+          <p className="mb-30 text-body02 text-gray-600">
+            엔빵을 이용하려면 다시 로그인해야 해요.
+          </p>
+        </div>
+        <div className="flex flex-row gap-10">
+          <button
+            onClick={onClose}
+            className="btn btn-small btn-secondary text-heading06"
+          >
+            취소
+          </button>
+          <button
+            onClick={onSubmit}
+            className="btn btn-small btn-warning text-heading06"
+          >
+            로그아웃
+          </button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
 
-export default LogoutModal;
+export default LogoutModal
