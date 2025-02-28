729c534ada9238f2717d0bf5956d5d9323125ef7
shinhm1 <shinhm1@naver.com>
2025-02-28 21:23:43 +0900

    feat: DetailHeader에 onClickBack props 추가

--- src/components/common/header/detailHeader.tsx
+++ src/components/common/header/detailHeader.tsx
@@ -3,12 +3,19 @@
 import { useRouter } from 'next/navigation'
 import Icon from '../icon/icon'
 
-export default function DetailHeader() {
+interface DetailHeaderProps {
+  onClickBack?: () => void
+}
+
+export default function DetailHeader({ onClickBack }: DetailHeaderProps) {
   const router = useRouter()
 
   return (
     <header className="h-48 w-full pt-16">
-      <div className="cursor-pointer" onClick={() => router.back()}>
+      <div
+        className="w-24 cursor-pointer"
+        onClick={() => (onClickBack ? onClickBack() : router.back())}
+      >
         <Icon type="angleLeft" width={16} height={16} fill="text-gray-600" />
       </div>
     </header>
