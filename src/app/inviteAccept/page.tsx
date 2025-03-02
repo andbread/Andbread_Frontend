85b0cd68dc1ebe39c03524cadb4647b7bc408d6f
shinhm1 <shinhm1@naver.com>
2025-03-02 23:23:57 +0900

    style: 엔빵 초대 페이지 퍼블리싱 수정

--- src/app/inviteAccept/page.tsx
+++ src/app/inviteAccept/page.tsx
@@ -1,14 +1,16 @@
-
-import InviteAcceptTitle from "@/components/inviteAccept/InviteAcceptTitle";
-import InviteAcceptButton from "@/components/inviteAccept/InviteAcceptButton";
+import InviteAcceptTitle from '@/components/inviteAccept/InviteAcceptTitle'
+import InviteAcceptButton from '@/components/inviteAccept/InviteAcceptButton'
+import NbreadsImage from '@/components/common/nbreadImage/NbreadsImage'
 
 const InviteAcceptPage = () => {
-   
-    return (
-       <div className="flex flex-col px-24">
-        <InviteAcceptTitle/>
-        <InviteAcceptButton/>
-       </div>
-    )
+  return (
+    <div className="flex h-full flex-col justify-between px-24">
+      <InviteAcceptTitle />
+      <div className="mt-20 flex w-full flex-col items-center justify-center">
+        <NbreadsImage isFloating={true} />
+      </div>
+      <InviteAcceptButton />
+    </div>
+  )
 }
-export default InviteAcceptPage;
+export default InviteAcceptPage
--- src/components/inviteAccept/InviteAcceptTitle.tsx
+++ src/components/inviteAccept/InviteAcceptTitle.tsx
@@ -1,19 +1,18 @@
-
-
 const InviteAcceptTitle = () => {
-   const data ={
-    id: "신혜민",
-    title: "유튜브 프리미엄"
-}
+  const data = {
+    id: '신혜민',
+    title: '유튜브 프리미엄',
+  }
   return (
-      <div>
-          <div className="mt-111 mb-20 text-heading02">
+    <div>
+      <div className="mb-20 mt-72 text-heading02">
         <span>{data.id}</span>님이 당신을
         <br />
         <span className="text-secondary-100">{data.title}</span>으로 초대했어요
       </div>
-      <div className='text-gray-400 text-body02 mb-371'>
-        엔빵 초대를 수락하고 친구들과<br/> 정기 결제 일정 및 금액을 공유해보세요.
+      <div className="text-body02 text-gray-400">
+        엔빵 초대를 수락하고 친구들과
+        <br /> 정기 결제 일정 및 금액을 공유해보세요.
       </div>
     </div>
   )
