4f142f28243d4e10f4405246a05514fae9683554
shinhm1 <shinhm1@naver.com>
2025-03-02 21:51:36 +0900

    fix: 환경변수 console.log 제거

--- src/lib/supabaseAdminClient.ts
+++ src/lib/supabaseAdminClient.ts
@@ -1,14 +1,11 @@
-
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
-const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string
+const supabaseServiceRoleKey = process.env
+  .NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string
 
 if (!supabaseServiceRoleKey) {
-    console.error("환경 변수가 올바르게 설정되지 않았습니다.");
+  console.error('환경 변수가 올바르게 설정되지 않았습니다.')
 }
 
-console.log("Supabase URL: ", supabaseUrl);
-console.log("supabaseKey: ", supabaseKey);
-console.log("Service Role Key: ", supabaseServiceRoleKey);
-export const adminSupabase = createClient(supabaseUrl,supabaseServiceRoleKey)
+export const adminSupabase = createClient(supabaseUrl, supabaseServiceRoleKey)
