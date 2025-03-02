cbb16873b4a7d69b3d59f1f886ebf018a344f380
shinhm1 <shinhm1@naver.com>
2025-03-02 21:51:13 +0900

    refactor: 로그아웃, 회원탈퇴 시 Spinner 및 Toast 메시지 출력

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,8 @@ import { adminSupabase } from './supabaseAdminClient'
 import { LoginProvider } from '@/types/user'
 import useUserStore from '@/stores/useAuthStore'
 import { useRouter } from 'next/navigation'
+import { useToast } from '@/components/common/toast/Toast'
+
 export const login = async (provider: LoginProvider['provider']) => {
   const redirectToUrl = process.env.NEXT_PUBLIC_REDIRECT_URL
   const { data, error } = await supabase.auth.signInWithOAuth({
@@ -22,23 +24,31 @@ export const login = async (provider: LoginProvider['provider']) => {
 
   return data
 }
+
 export const logout = async (router: ReturnType<typeof useRouter>) => {
   const data = await supabase.auth.signOut()
   console.log(data)
   useUserStore.getState().clearUser()
   sessionStorage.removeItem('user-store')
-  router.replace('/login')
+  setTimeout(() => {
+    useToast.success('로그아웃이 완료되었어요.')
+    router.replace('/login')
+  }, 1000)
 }
+
 export const deleteAccount = async (router: ReturnType<typeof useRouter>) => {
-    const data = await supabase.auth.getUser();
-    console.log(data.data.user?.id);
-    const user = data.data.user?.id;
-    if(!user) {
-        throw new Error("유저정보를 찾을수 없음");
-    }
-    const deleteUser = await adminSupabase.auth.admin.deleteUser(user);
-    useUserStore.getState().clearUser()
+  const data = await supabase.auth.getUser()
+  console.log(data.data.user?.id)
+  const user = data.data.user?.id
+  if (!user) {
+    throw new Error('유저정보를 찾을수 없음')
+  }
+  const deleteUser = await adminSupabase.auth.admin.deleteUser(user)
+  useUserStore.getState().clearUser()
   sessionStorage.removeItem('user-store')
-  await supabase.auth.signOut();
-  router.replace('/login')
+  await supabase.auth.signOut()
+  setTimeout(() => {
+    useToast.success('회원 탈퇴가 완료되었어요.')
+    router.replace('/login')
+  }, 1000)
 }
