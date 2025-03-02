b163a1400e5c8f65d47ce46f716e421c07e0044e
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2025-03-02 20:52:40 +0900

    feat : 콜백 페이지 로딩스피너 추가

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,8 +1,16 @@
-"use client";
+
+"use client";  
+
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 import useUserStore from '@/stores/useAuthStore';
 import { useRouter } from 'next/navigation';
+import NProgress from 'nprogress'; 
+import 'nprogress/nprogress.css'; 
+
+// NProgress 스피너 비활성화
+NProgress.configure({ showSpinner: false });
+
 const CallbackPage = () => {
   const [loading, setLoading] = useState(true);
   const setUser = useUserStore((state) => state.setUser);
@@ -11,6 +19,8 @@ const CallbackPage = () => {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
+        NProgress.start();  // 로딩 시작
+
         const { data, error } = await supabase.auth.getUser();
 
         if (error) {
@@ -24,26 +34,35 @@ const CallbackPage = () => {
           const userInfo = {
             id: data.user.id,
             email: data.user.email || '',
-            socialType : provider,
+            socialType: provider,
             name: data.user.user_metadata.full_name || '',
             profileImage: data.user.user_metadata.avatar_url || '',
           };
           setUser(userInfo); // 사용자 정보 세션 스토리지에 저장
-          router.replace('/home');
+          setTimeout(() => {
+            setLoading(false);
+            router.replace('/home');
+          }, 1000);
         }
       } catch (error) {
         console.error('Error fetching user data:', error);
       } finally {
-        setLoading(false);
+        NProgress.done();  // 로딩 완료
       }
     };
 
     fetchUserData();
-  }, [router,setUser]);
+  }, [router, setUser]);
 
-
-
-  return <div>{loading ? 'Loading...' : 'Redirecting...'}</div>;
+  return (
+    <div>
+      {loading ? (
+        <div className="spinner-overlay">
+          <div className="spinner"></div>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default CallbackPage;
