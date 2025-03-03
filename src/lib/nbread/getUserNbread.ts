16f4912e6794c7671196502c76e6676e5685a3a3
shinhm1 <shinhm1@naver.com>
2025-03-03 19:42:10 +0900

    fix: 홈페이지 납부 완료/미완료 카운트 오류 해결

--- src/lib/nbread/getUserNbread.ts
+++ src/lib/nbread/getUserNbread.ts
@@ -5,6 +5,7 @@ import { NbreadRow } from '@/types/supabase'
 export const getUserNbreads = async (userId: string): Promise<Nbread[]> => {
   if (!userId) return []
 
+  // 1. 현재 로그인한 유저의 참여 정보를 가져옴
   const { data: participantEntries, error: participantError } = await supabase
     .from('participant')
     .select('nbread_id')
@@ -18,14 +19,11 @@ export const getUserNbreads = async (userId: string): Promise<Nbread[]> => {
     return []
   }
 
-  console.log(`✅ Retrieved participant entries:`, participantEntries)
-
+  // 2. 현재 로그인한 유저가 참여 중인 엔빵 정보를 가져옴
   const nbreadIds = participantEntries.map((entry) => entry.nbread_id)
-
   if (nbreadIds.length === 0) {
     return []
   }
-
   const { data: nbreads, error } = await supabase
     .from('nbread')
     .select('*')
@@ -36,25 +34,7 @@ export const getUserNbreads = async (userId: string): Promise<Nbread[]> => {
     return []
   }
 
-  // 각 엔빵별 결제 완료 인원 수 조회
-  const paidCounts = await Promise.all(
-    nbreadIds.map(async (nbreadId) => {
-      const { count, error } = await supabase
-        .from('nbread_records')
-        .select('*', { count: 'exact' })
-        .eq('nbread_id', nbreadId)
-        .eq('is_paid', true)
-
-      if (error) {
-        console.error(`❌ Failed to fetch paid count for nbread_id: ${nbreadId}`, error.message)
-        return 0
-      }
-
-      return count || 0
-    })
-  )
-
-  // Supabase에서 가져온 데이터를 type에 맞게 변환
+  // 3. Supabase에서 가져온 엔빵 정보를 type에 맞게 변환
   const renamedNbreadsData: Nbread[] = (nbreads as NbreadRow[])?.map(
     (nbread, index) => ({
       id: nbread.id,
@@ -67,9 +47,30 @@ export const getUserNbreads = async (userId: string): Promise<Nbread[]> => {
       leaderId: nbread.leader_id,
       participants: null,
       currentPaymentDate: nbread.current_payment_date,
-      paidCount: paidCounts[index],
     }),
   )
 
-  return renamedNbreadsData
+  // 4. 각 nbread 객체에 paidCount 값을 추가
+  const nbreadWithPaidCounts = await Promise.all(
+    renamedNbreadsData.map(async (nbread) => {
+      const { count, error } = await supabase
+        .from('nbread_records')
+        .select('*', { count: 'exact' })
+        .eq('nbread_id', nbread.id)
+        .eq('payment_date', nbread.currentPaymentDate)
+        .eq('is_paid', true)
+
+      if (error) {
+        console.error(
+          `❌ Failed to fetch paid count for nbread_id: ${nbread.id}`,
+          error.message,
+        )
+        return { ...nbread, paidCount: 0 }
+      }
+
+      return { ...nbread, paidCount: count || 0 }
+    }),
+  )
+
+  return nbreadWithPaidCounts
 }
