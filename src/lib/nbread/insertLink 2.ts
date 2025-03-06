5f3393d1d6eedb7fb193bcc44c95600201c95277
shinhm1 <shinhm1@naver.com>
2025-03-07 02:41:17 +0900

    chore: 중복 파일 제거

--- src/lib/nbread/insertLink 2.ts
+++
@@ -1,9 +0,0 @@
-export const createLink = (nbreadId: string) => {
-  const baseUrl = 'http://localhost:3000/inviteAccept'
-
-  const uniqueCode = Math.random().toString(36).substr(2, 9)
-
-  const inviteLink = `${baseUrl}?groupId=${nbreadId}&code=${uniqueCode}`
-
-  return inviteLink
-}
