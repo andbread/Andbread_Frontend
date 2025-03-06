8c9b966f36ae6e64cb4f5559c43c9f6ad88b1a4d
shinhm1 <shinhm1@naver.com>
2025-03-06 14:35:17 +0900

    style: 마이페이지 퍼블리싱 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -16,20 +16,18 @@ const MyPage = () => {
 
   return (
     <div className="flex flex-col justify-between p-24">
-      <DetailHeader></DetailHeader>
-      <main className="mt-24 p-4">
+      <DetailHeader />
+      <main className="mt-24">
         <section>
-          <h2 className="mb-24 text-heading04 font-bold text-gray-800">
-            내 정보
-          </h2>
+          <h2 className="mb-12 text-heading02 text-gray-800">내 정보</h2>
           <MyInfo />
         </section>
         <section>
-          <h2 className="mb-24 mt-24 text-body03 text-gray-500">기타</h2>
+          <div className="mb-12 mt-40 text-body02 text-gray-500">기타</div>
           <MenuList />
         </section>
         <section>
-          <h2 className="mb-24 mt-24 text-body03 text-gray-500">계정 관리</h2>
+          <h2 className="mb-12 mt-40 text-body02 text-gray-500">계정 관리</h2>
           <Manage setAuthProgress={setAuthProgress} />
         </section>
       </main>
--- src/components/mypage/MenuList.tsx
+++ src/components/mypage/MenuList.tsx
@@ -1,25 +1,31 @@
-"use client";
+'use client'
 
-import { useState } from "react";
-import MyNbreadSumModal from "@/components/common/modal/MyNbreadSumModal";
+import { useState } from 'react'
+import MyNbreadSumModal from '@/components/common/modal/MyNbreadSumModal'
 
 const MenuList = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-    return (
-      <div className="card p-24 mt-4">
-        <ul className="py-2">
-          <li className="py-10 text-body02 text-gray-800 cursor-pointer" onClick={() => setIsModalOpen(true)}>나의 엔빵 합계 확인하기</li>
-          <li className="py-10 text-body02 text-gray-800 cursor-pointer">개발진에게 의견 보내기</li>
-        </ul>
-        
-        <MyNbreadSumModal 
-        isOpen={isModalOpen} 
-        onClose={() => setIsModalOpen(false)} 
+  return (
+    <div className="card p-28">
+      <ul>
+        <li
+          className="mb-20 cursor-pointer text-body02 text-gray-800"
+          onClick={() => setIsModalOpen(true)}
+        >
+          나의 엔빵 합계 확인하기
+        </li>
+        <li className="cursor-pointer text-body02 text-gray-800">
+          개발진에게 의견 보내기
+        </li>
+      </ul>
+
+      <MyNbreadSumModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
       />
-      </div>
-    );
-  };
-  
-  export default MenuList;
-  
+    </div>
+  )
+}
+
+export default MenuList
