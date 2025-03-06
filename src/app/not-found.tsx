b7bf3f801d7b54734378a3fe3b65ac19eb556bad
shinhm1 <shinhm1@naver.com>
2025-03-06 16:00:19 +0900

    feat: not found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import NbreadNotFound from '@/assets/logo/nbreads-not-found.svg'
+import { useRouter } from 'next/navigation'
+
+const Page = () => {
+  const router = useRouter()
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center gap-20">
+      <h1 className="text-64 text-gray-600">404</h1>
+      <h2 className="text-gray-600">NOT FOUND</h2>
+      <div className="mb-40 text-body01 text-gray-400">
+        요청하신 페이지를 찾을 수 없어요.
+      </div>
+      <NbreadNotFound />
+      <div
+        className="mt-64 cursor-pointer text-body01 text-gray-700"
+        onClick={() => router.replace('/home')}
+      >
+        홈으로 가기
+      </div>
+    </div>
+  )
+}
+
+export default Page
