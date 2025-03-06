0b4158244d700549593ef35d5e6839584bb5df27
shinhm1 <shinhm1@naver.com>
2025-03-06 14:23:44 +0900

    style: 로그인 페이지 퍼블리싱 수정

--- src/components/user/LoginButton.tsx
+++ src/components/user/LoginButton.tsx
@@ -1,31 +1,31 @@
-"use client"; 
-import Icon from '../common/icon/Icon';
+'use client'
+import Icon from '../common/icon/Icon'
 import { login } from '@/lib/auth'
-import { LoginProvider } from '@/types/user';
+import { LoginProvider } from '@/types/user'
 
-const providerStyles  = {
+const providerStyles = {
   kakao: ' bg-[#FEE500] hover:bg-yellow-400 text-black',
-  google: 'bg-[#F2F2F2]  hover:bg-gray-200 text-black shadow-lg',
+  google:
+    'bg-[#F2F2F2] hover:bg-gray-200 text-black border-2 border-solid border-[#E7E7E7]',
 }
 const providerText = {
   google: '구글로 시작하기',
   kakao: '카카오로 시작하기',
 }
 const handleLogin = async (provider: LoginProvider['provider']) => {
-    try {
-        // console.log('🔹 로그인 시도:', provider)
-        await login(provider);
-        // console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
-    
+  try {
+    // console.log('🔹 로그인 시도:', provider)
+    await login(provider)
+    // console.log(data);
+  } catch (error) {
+    console.error(error)
+  }
 }
 const LoginButton = ({ provider }: LoginProvider) => {
   return (
     <button
-      className={`h-[62px] w-[272px] rounded py-2 ${providerStyles[provider]} flex items-center justify-center rounded-lg text-heading04 transition-all duration-300`}
-        onClick={()=> handleLogin(provider)}
+      className={`h-56 w-272 rounded ${providerStyles[provider]} flex items-center justify-center rounded-8 pt-2 text-heading04 transition-all duration-300`}
+      onClick={() => handleLogin(provider)}
     >
       <Icon
         type={provider === 'kakao' ? 'kakaoLogo' : 'googleLogo'}
