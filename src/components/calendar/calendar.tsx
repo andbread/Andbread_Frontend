96dd0cd7693b3784f77f9878d72f9693944d7273
shinhm1 <shinhm1@naver.com>
2025-03-06 15:41:55 +0900

    style: 캘린더 페이지 퍼블리싱 수정

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react"
-import Icon from "../common/icon/Icon"
+import { useEffect, useState } from 'react'
+import Icon from '../common/icon/Icon'
 
-import { fetchNbreadData } from "@/lib/nbread/fetchNbreadData"
+import { fetchNbreadData } from '@/lib/nbread/fetchNbreadData'
 
-const daysOfWeek = ["월", "화", "수", "목", "금", "토", "일"]
+const daysOfWeek = ['월', '화', '수', '목', '금', '토', '일']
 
 interface CalendarProps {
   currentDate: Date
@@ -12,7 +12,12 @@ interface CalendarProps {
   onDateSelect: (date: Date) => void
 }
 
-const Calendar: React.FC<CalendarProps> = ({ currentDate, onMonthChange, userId, onDateSelect }) => {
+const Calendar: React.FC<CalendarProps> = ({
+  currentDate,
+  onMonthChange,
+  userId,
+  onDateSelect,
+}) => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
   const [paymentDates, setPaymentDates] = useState<number[]>([])
 
@@ -23,12 +28,16 @@ const Calendar: React.FC<CalendarProps> = ({ currentDate, onMonthChange, userId,
   const lastDate = new Date(year, month + 1, 0).getDate()
 
   const prevLastDate = new Date(year, month, 0).getDate()
-  const prevDates = Array.from({ length: firstDay }, (_, i) => prevLastDate - firstDay + i + 1)
+  const prevDates = Array.from(
+    { length: firstDay },
+    (_, i) => prevLastDate - firstDay + i + 1,
+  )
   const currentDates = Array.from({ length: lastDate }, (_, i) => i + 1)
   const totalDays = prevDates.length + currentDates.length
-  const nextDates = Array.from({ length: totalDays % 7 ? 7 - (totalDays % 7) : 0 }, (_, i) => i + 1)
-
-  
+  const nextDates = Array.from(
+    { length: totalDays % 7 ? 7 - (totalDays % 7) : 0 },
+    (_, i) => i + 1,
+  )
 
   const handleDateClick = (date: Date) => {
     setSelectedDate(date)
@@ -47,7 +56,11 @@ const Calendar: React.FC<CalendarProps> = ({ currentDate, onMonthChange, userId,
 
   const isToday = (day: number) => {
     const today = new Date()
-    return today.getDate() === day && today.getMonth() === month && today.getFullYear() === year
+    return (
+      today.getDate() === day &&
+      today.getMonth() === month &&
+      today.getFullYear() === year
+    )
   }
 
   useEffect(() => {
@@ -61,45 +74,64 @@ const Calendar: React.FC<CalendarProps> = ({ currentDate, onMonthChange, userId,
           .map((item) => {
             const paymentDate = item.payment_date
             if (paymentDate instanceof Date) {
-              return paymentDate.getMonth() === month ? paymentDate.getDate() : null
+              return paymentDate.getMonth() === month
+                ? paymentDate.getDate()
+                : null
             }
-            if (typeof paymentDate === "string") {
+            if (typeof paymentDate === 'string') {
               const parsedDate = new Date(paymentDate)
-              return parsedDate.getMonth() === month ? parsedDate.getDate() : null
+              return parsedDate.getMonth() === month
+                ? parsedDate.getDate()
+                : null
             }
             return null
           })
           .filter((date) => date !== null)
         setPaymentDates(payDates as number[])
       }
-    };
+    }
 
     fetchData()
   }, [userId, month])
 
   return (
-    <div className="w-full h-[390px] bg-white pt-[16px] mb-[20px]">
-      <div className="flex justify-between items-center mb-[20px]">
-        <div className="pl-[24px] cursor-pointer">
-          <Icon type="angleLeft" onClick={prevMonth} width="10" height="10" />
+    <div className="shadow-card mb-20 h-auto w-full bg-white pt-20">
+      <div className="mb-32 flex items-center justify-between">
+        <div className="cursor-pointer pl-28">
+          <Icon
+            type="angleLeft"
+            onClick={prevMonth}
+            width="12"
+            height="12"
+            fill={'text-gray-400'}
+          />
         </div>
         <h2 className="text-body-04 gray-400">{month + 1}월</h2>
-        <div className="pr-[24px] cursor-pointer">
-          <Icon type="angleRight" onClick={nextMonth} width="10" height="10" />
+        <div className="cursor-pointer pr-28">
+          <Icon
+            type="angleRight"
+            onClick={nextMonth}
+            width="12"
+            height="12"
+            fill={'text-gray-400'}
+          />
         </div>
       </div>
 
-      <div className="mt-4 mb-[12px] grid grid-cols-7 gap-1 text-center text-body-04 text-gray-400">
+      <div className="text-body-04 mb-24 grid grid-cols-7 px-12 text-center text-gray-400">
         {daysOfWeek.map((day, index) => (
-          <div key={day} className={`p-1 ${index === 6 ? "text-system-red" : ""}`}>
+          <div
+            key={day}
+            className={`p-1 ${index === 6 ? 'text-system-red' : ''}`}
+          >
             {day}
           </div>
         ))}
       </div>
 
-      <div className="grid grid-cols-7 gap-1 text-center mb-[16px]">
+      <div className="mb-20 grid grid-cols-7 px-12 text-center">
         {prevDates.map((day, index) => (
-          <div key={`prev-${index}`} className="p-2 text-gray-400 mb-[16px]">
+          <div key={`prev-${index}`} className="mb-16 p-2 text-gray-400">
             {day}
           </div>
         ))}
@@ -107,25 +139,25 @@ const Calendar: React.FC<CalendarProps> = ({ currentDate, onMonthChange, userId,
         {currentDates.map((day) => (
           <div
             key={day}
-            className={`relative w-[32px] h-[32px] mx-auto mb-[16px] flex flex-col items-center justify-center cursor-pointer rounded-full ${
+            className={`relative mx-auto mb-16 flex h-32 w-32 cursor-pointer flex-col items-center justify-center rounded-full ${
               selectedDate?.getDate() === day &&
               selectedDate?.getMonth() === month
-                ? "bg-secondary-100"
+                ? 'bg-secondary-100'
                 : isToday(day)
-                ? "bg-primary-200"
-                : "hover:bg-secondary-100"
+                  ? 'bg-primary-200'
+                  : 'hover:bg-secondary-100'
             }`}
             onClick={() => handleDateClick(new Date(year, month, day))}
           >
             {day}
             {paymentDates.includes(day) && (
-              <div className="w-[6px] h-[6px] bg-primary-500 rounded-full mt-1"></div>
+              <div className="mt-1 h-[6px] w-[6px] rounded-full bg-primary-500"></div>
             )}
           </div>
         ))}
 
         {nextDates.map((day, index) => (
-          <div key={`next-${index}`} className="p-2 text-gray-400 mb-[16px]">
+          <div key={`next-${index}`} className="p-2 text-gray-400">
             {day}
           </div>
         ))}
