059844ee43c0266019a6c12fa3855e29ed34f100
shinhm1 <shinhm1@naver.com>
2025-03-08 20:56:19 +0900

    fix: 컬러코드 하드코딩 수정 및 버튼 클래스 재사용

--- src/components/common/Modal/MyNbreadSumModal.tsx
+++ src/components/common/Modal/MyNbreadSumModal.tsx
@@ -1,43 +1,40 @@
-import { useEffect, useState } from "react";
-import Modal from "./Modal";
-import { getUserTotalNbreadAmount } from "@/lib/nbread/getUserTotalNbreadAmount";
-import useUserStore from "@/stores/useAuthStore";
+import { useEffect, useState } from 'react'
+import Modal from './Modal'
+import { getUserTotalNbreadAmount } from '@/lib/nbread/getUserTotalNbreadAmount'
+import useUserStore from '@/stores/useAuthStore'
 
 interface MyNbreadSumModalProps {
-  isOpen: boolean;
-  onClose: () => void;
+  isOpen: boolean
+  onClose: () => void
 }
 
 const MyNbreadSumModal = ({ isOpen, onClose }: MyNbreadSumModalProps) => {
-  const [totalAmount, setTotalAmount] = useState(0);
-  const userData = useUserStore((state) => state.user);
+  const [totalAmount, setTotalAmount] = useState(0)
+  const userData = useUserStore((state) => state.user)
 
   useEffect(() => {
     if (userData?.id) {
       const fetchTotalAmount = async () => {
-        const total = await getUserTotalNbreadAmount(userData.id);
-        setTotalAmount(total);
-      };
-      fetchTotalAmount();
+        const total = await getUserTotalNbreadAmount(userData.id)
+        setTotalAmount(total)
+      }
+      fetchTotalAmount()
     }
-  }, [userData?.id]);
+  }, [userData?.id])
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <p className="text-heading04 font-bold text-gray-800 text-center">
+      <p className="text-center text-heading04 font-bold text-gray-800">
         내가 그동안 엔빵한 총 금액은?
       </p>
-      <p className="text-heading01 font-extrabold text-[#FF8204] text-center mt-24">
+      <p className="mt-24 text-center text-heading01 text-secondary-300">
         {totalAmount.toLocaleString()}원
       </p>
-      <button
-        onClick={onClose}
-        className="bg-[#FFAC39] text-white text-lg font-semibold w-[232px] h-[48px] p-12 m-16 rounded-md"
-      >
+      <button onClick={onClose} className="btn btn-primary btn-medium">
         알겠어요
       </button>
     </Modal>
-  );
-};
+  )
+}
 
-export default MyNbreadSumModal;
+export default MyNbreadSumModal
