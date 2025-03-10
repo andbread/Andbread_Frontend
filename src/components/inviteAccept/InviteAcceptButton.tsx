d1191913657d596ae76e3ce314090cff0d277618
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2025-03-10 15:15:12 +0900

    refactor : 초대 수락페이지 수정

--- src/components/inviteAccept/InviteAcceptButton.tsx
+++ src/components/inviteAccept/InviteAcceptButton.tsx
@@ -1,17 +1,34 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InviteAcceptModal from './InviteAcceptModal'
 import { useRouter } from 'next/navigation'
+
 const InviteAcceptButton = () => {
   const [isModalOpen, setModalOpen] = useState(false)
   const router = useRouter()
+  const [isLogin, setIsLogin] = useState(false)
+
   const handleModalClose = () => {
     setModalOpen(false)
   }
+
   const handleHome = () => {
-    
     router.push('/home')
   }
+
+  const handleLogin = () => {
+    router.push('/login')
+  }
+
+  useEffect(() => {
+    const user = sessionStorage.getItem('user-store')
+    if (user) {
+      setIsLogin(true) 
+    } else {
+      setIsLogin(false) 
+    }
+  }, []) 
+
   return (
     <div className="mb-64 flex flex-col">
       <button
@@ -20,11 +37,20 @@ const InviteAcceptButton = () => {
       >
         초대 수락하기 🍞
       </button>
-      <button className="text-body02 text-gray-600" onClick={handleHome}>
-        홈으로 가기
-      </button>
+      
+      {isLogin ? (
+        <button className="text-body02 text-gray-600" onClick={handleHome}>
+          홈으로 가기
+        </button>
+      ) : (
+        <button className="text-body02 text-gray-600" onClick={handleLogin}>
+          로그인하러 가기
+        </button>
+      )}
+
       <InviteAcceptModal isOpen={isModalOpen} onClose={handleModalClose} />
     </div>
   )
 }
+
 export default InviteAcceptButton
