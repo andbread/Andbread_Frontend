3b9ad27ade2e2cb314950336d19e3fa3ea83f6e9
subinsong01 <haely97@naver.com>
2025-04-05 18:42:56 +0900

    style: 커뮤니티 페이지 1차 완성

---
+++ src/app/community/main/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import DetailHeader from '@/components/common/header/DetailHeader'
+import PostBoard from '@/components/community/PostBoard'
+const CommunityPage = () => {
+  return (
+    <div className="flex flex-col justify-between p-24">
+      <DetailHeader />
+      <PostBoard />
+    </div>
+  )
+}
+
+export default CommunityPage
