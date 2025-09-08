6842a194526252f92f683c8494e43883c8767fac
Hyun9811 <145583411+Hyun9811@users.noreply.github.com>
2025-09-08 14:23:00 +0900

    feat : 엔빵초대 바텀시트 검색기능 구현

--- src/components/invite/InviteUserListItem.tsx
+++ src/components/invite/InviteUserListItem.tsx
@@ -1,38 +1,52 @@
 import DefaultAvatar from '@/assets/avatar.svg'
+import Avatar from '../common/avatar/avatar'
 interface InviteUserData {
-    avatar : string
-    name : string
-    status : string
+  avatar: string | null
+  name: string
+  status: string
 }
-const InviteUserListItem = ({avatar,name,status} : InviteUserData) => {
-    const getInviteUserStatus = (status : string) => {
-        switch (status) {
-            case '초대 하기':
-              return { color: 'text-system-blue01', cursor: 'cursor-pointer' };
-            case '참여 중':
-              return { color: 'text-gray-400', cursor: null };
-            case '초대 완료':
-              return { color: 'text-gray-400', cursor: null };
-            default:
-              return { color: 'text-black', cursor: 'cursor-pointer' };
-          }   
-        };
-        const {color,cursor} = getInviteUserStatus(status);
+const InviteUserListItem = ({ avatar, name, status }: InviteUserData) => {
+  const getInviteUserStatus = (status: string) => {
+    switch (status) {
+      case '초대 하기':
+        return { color: 'text-system-blue01', cursor: 'cursor-pointer' }
+      case '참여 중':
+        return { color: 'text-gray-400', cursor: null }
+      case '초대 완료':
+        return { color: 'text-gray-400', cursor: null }
+      default:
+        return { color: 'text-black', cursor: 'cursor-pointer' }
+    }
+  }
+  const { color, cursor } = getInviteUserStatus(status)
 
-        const handleClick = (status : string) => {
-            if(status == '초대 하기'){
-                console.log('초대 보냄');
-            }
-        }
+  const handleClick = (status: string) => {
+    if (status == '초대 하기') {
+      console.log('초대 보냄')
+    }
+  }
   return (
     <div className="flex w-full flex-row pb-[30px]">
-      <div className="flex flex-row items-center w-[40%] gap-[20px]">
-        <DefaultAvatar />
+      <div className="flex w-[40%] flex-row items-center gap-[20px]">
+        {/* <Avatar size='large' profileImageUrl={avatar}/> */}
+        {avatar && typeof avatar === 'string' && avatar !== '' ? (
+    <img
+      src={avatar}
+      alt={name}
+      className="h-32 w-32 rounded-full object-cover"
+    />
+  ) : (
+    <DefaultAvatar className="h-32 w-32" />
+  )}
         <p className="text-body01">{name}</p>
       </div>
-      <div className='flex items-center w-[60%] justify-end'>
-        <p className={`${color} text-body01 ${cursor}`}
-        onClick={() => handleClick(status)}>{status}</p>
+      <div className="flex w-[60%] items-center justify-end">
+        <p
+          className={`${color} text-body01 ${cursor}`}
+          onClick={() => handleClick(status)}
+        >
+          {status}
+        </p>
       </div>
     </div>
   )
