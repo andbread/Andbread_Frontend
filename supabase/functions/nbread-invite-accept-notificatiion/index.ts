8a3dc16441e70d619bac41aff7fbe441c922503f
shinhm1 <shinhm1@naver.com>
2025-11-03 14:36:24 +0900

    fix: 엔빵 초대/수락 알림 type 수정

--- supabase/functions/nbread-invite-accept-notificatiion/index.ts
+++ supabase/functions/nbread-invite-accept-notificatiion/index.ts
@@ -153,7 +153,7 @@ Deno.serve(async (req) => {
         message,
         title,
         is_read: false,
-        type: 'payment',
+        type: 'invite_accept',
         data: {
           nbreadId: nbreadId,
         },
