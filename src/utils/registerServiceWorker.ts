86fa1097a2a8c3092014be4f19c71cdd4f80257f
shinhm1 <shinhm1@naver.com>
2025-11-16 19:47:40 +0900

    chore: alert 제거

--- src/utils/registerServiceWorker.ts
+++ src/utils/registerServiceWorker.ts
@@ -5,7 +5,6 @@ export const registerServiceWorker = async () => {
         '/firebase-messaging-sw.js',
       )
       console.log('service worker 등록 완료: ', registration)
-      alert('service worker 등록 완료')
       return registration
     } catch (error) {
       console.error('service worker 등록 실패: ', error)
@@ -13,7 +12,6 @@ export const registerServiceWorker = async () => {
     }
   } else {
     console.log('service worker가 지원되지 않음')
-    alert('service worker 미지원')
     return Promise.reject('service worker가 지원되지 않음')
   }
 }
