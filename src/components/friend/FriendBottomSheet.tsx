0f5c733a8d9564bb162e2cb5f2fb71022918f8ba
shinhm1 <shinhm1@naver.com>
2026-05-18 22:33:56 +0900

    style: 친구 카드와 입력 아이콘 UI 정리

--- src/components/friend/FriendBottomSheet.tsx
+++ src/components/friend/FriendBottomSheet.tsx
@@ -4,7 +4,7 @@ import Icon from '../common/icon/Icon'
 interface FriendBottoSheetProps {
   profile: string
   name: string
-  tag: number
+  tag: string
   isOpen: boolean
   onClose: () => void
 }
@@ -15,12 +15,6 @@ const FriendBottomSheet = ({
   name,
   profile,
 }: FriendBottoSheetProps) => {
-    const handleFollow = () =>{
-        console.log("팔로우 버튼")
-    }
-    const handleInvited = () =>{
-        console.log("초대하기 버튼")
-    }
   return (
     <>
       <BottomSheet isOpen={isOpen} onClose={onClose}>
@@ -34,20 +28,26 @@ const FriendBottomSheet = ({
           ) : (
             <DefaultAvatar className="h-96 w-96 rounded-full object-cover" />
           )}
-          <div className="flex flex-row items-center gap-8 mb-69">
+          <div className="mb-69 flex flex-row items-center gap-8">
             <h1 className="text-heading-02">{name}</h1>
             <p className="text-body-01 text-secondary-100">#{tag}</p>
           </div>
         </div>
-        <div className='flex flex-row justify-center gap-8 mb-30'>
-          <button className="btn-small bg-system-blue01 text-white flex flex-row items-center justify-center gap-8 rounded-8"
-          onClick={handleFollow}>
-            <Icon type="profile" width={12} height={12}/>
-            팔로우 하기</button>
-          <button className="btn-small bg-secondary-100 text-white flex flex-row items-center justify-center gap-8 rounded-8"
-          onClick={handleInvited}>
-            <Icon type="plus" width={12} height={12}/>
-            초대하기</button>
+        <div className="mb-30 flex flex-row justify-center gap-8">
+          <button
+            className="btn-small flex flex-row items-center justify-center gap-8 rounded-8 bg-system-blue01 text-white"
+            onClick={() => null}
+          >
+            <Icon type="profile" fill="text-white" width={12} height={12} />
+            팔로우 하기
+          </button>
+          <button
+            className="btn-small flex flex-row items-center justify-center gap-8 rounded-8 bg-secondary-100 text-white"
+            onClick={() => null}
+          >
+            <Icon type="plus" width={12} height={12} />
+            초대하기
+          </button>
         </div>
       </BottomSheet>
     </>
