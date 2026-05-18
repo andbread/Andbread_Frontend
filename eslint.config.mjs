368b65882722bef97d5dace13ac79047cf848784
shinhm1 <shinhm1@naver.com>
2026-05-19 01:41:34 +0900

    fix: Preview 빌드 ESLint 설정 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import prettier from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -11,14 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-
-  {
-    extends: ['prettier'],
-  },
-
-  {
-    plugins: ['tailwindcss'],
-  },
+  prettier,
 ]
 
 export default eslintConfig
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
 import { withSentryConfig } from '@sentry/nextjs'
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
